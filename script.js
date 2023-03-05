781cfc7cecdab5bf8eb0120bfbfcc70225e25a05
Jacob Wigent <jwigent18@gmail.com>
2023-03-05 10:43:34 -0500

    Added fade-out to about section

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const heroSection = document.getElementById("hero");
+const scrollIndicator = document.getElementById("scrollIndicator");
+
+const aboutSection = document.getElementById("about");
 
 const gameImage = document.getElementById("gameImage");
 const leavesImage = document.getElementById("leavesImage");
@@ -6,17 +8,29 @@ const gameStart = 100;
 const leavesStart = 100;
 const gameEnd = 112;
 const leavesEnd = 175;
-
 const logoImage = document.getElementById("logo");
+
+const aboutInfo = document.getElementById("about-info");
 
 
 addEventListener('scroll', e => {
     const scrollPercent = window.scrollY/window.innerHeight;
+    scrollIndicator.innerText = Math.round(scrollPercent*100) + "%";
 
-    leavesImage.style.backgroundSize = lerp(leavesStart, leavesEnd, scrollPercent) + "%";
-    gameImage.style.backgroundSize = lerp(gameStart, gameEnd, scrollPercent) + "%";
+    if (scrollPercent < 1) {
+        aboutSection.style.opacity = 1;
+        leavesImage.style.backgroundSize = lerp(leavesStart, leavesEnd, scrollPercent) + "%";
+        gameImage.style.backgroundSize = lerp(gameStart, gameEnd, scrollPercent) + "%";
+        logoImage.style.opacity = lerp(1, -0.8, scrollPercent);
+        aboutInfo.style.opacity = lerp(-1.2, 1, scrollPercent);
+    }
+    else if (scrollPercent < 2) {
+        const scrollPercent2 = scrollPercent%1;
 
-    logoImage.style.opacity = lerp(1, -0.8, scrollPercent);
+        aboutInfo.style.opacity = lerp(1, -3, scrollPercent2)
+        aboutSection.style.opacity = lerp(1, 0, scrollPercent2);
+    }
+
 })
 
 function lerp (start, end, amt){
